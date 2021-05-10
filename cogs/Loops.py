c0d0df9c8294402253114df06e17102891e398b2
Emanuele Borghini <manuborg2002@gmail.com>
2021-05-10 21:23:46 +0200

    added some extensions to fabio

--- cogs/Loops.py
+++ cogs/Loops.py
@@ -17,7 +17,7 @@ class Loops(commands.Cog):
     async def fabio(self):
         fabio = get(self.bot.get_all_members(), id=192261771380260864)
 
-        estensioni = [".7z", ".aac", ".apk", ".appx", ".arc", ".ass", ".bin", ".c", ".xaml", ".deb", ".dn", ".egg", ".exe", ".gbp", ".gbs", ".gif", ".gzip", ".html", ".jpg", ".jar", ".oar", ".osz", ".pak", ".php", ".pyk", ".py", ".pyw", ".rar", ".sb", ".tar", ".uha", ".viv", ".zip", ".iso", ".img", ".cad", ".dwg", ".gba", ".std", ".js", ".css", ".psd", ".ans", ".asc", ".doc", ".docx", ".log", ".pdf", ".xml", ".xhtml", ".xps", ".ico", ".bmp", ".jpeg", ".png", ".sym", ".url", ".dos", ".root", ".bat", ".cpp", ".c#", ".lua", ".obj", ".wav", ".mpeg", ".avi", ".flv", ".ogg", ".webm", ".nds", ".3ds", ".cia", ".cur", ".bak", ".raw", ".borgo", ""]
+        estensioni = [".azz",".storto",".doblo'",".arrosto",".cozze",".sfiga",".polentone",".dove",".7z", ".aac", ".apk", ".appx", ".arc", ".ass", ".bin", ".c", ".xaml", ".deb", ".dn", ".egg", ".exe", ".gbp", ".gbs", ".gif", ".gzip", ".html", ".jpg", ".jar", ".oar", ".osz", ".pak", ".php", ".pyk", ".py", ".pyw", ".rar", ".sb", ".tar", ".uha", ".viv", ".zip", ".iso", ".img", ".cad", ".dwg", ".gba", ".std", ".js", ".css", ".psd", ".ans", ".asc", ".doc", ".docx", ".log", ".pdf", ".xml", ".xhtml", ".xps", ".ico", ".bmp", ".jpeg", ".png", ".sym", ".url", ".dos", ".root", ".bat", ".cpp", ".c#", ".lua", ".obj", ".wav", ".mpeg", ".avi", ".flv", ".ogg", ".webm", ".nds", ".3ds", ".cia", ".cur", ".bak", ".raw", ".borgo", ""]
 
         await fabio.edit(nick=fabio.display_name.split(".")[0]+random.choice(estensioni))
 
