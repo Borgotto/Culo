f5a5f51fa3fe5e0d4ffc91422662047a3390cff1
Emanuele Borghini <manuborg2002@gmail.com>
2021-05-14 20:03:48 +0200

    boh

--- cogs/Musica.py
+++ cogs/Musica.py
@@ -16,13 +16,18 @@ class Music(commands.Cog):
     @commands.command(name="play")
     async def play(self, ctx, canale: VoiceChannel=None):
         if canale is None:
-            canale = ctx.author.voice.channel
+            if ctx.author.voice is None:
+                await ctx.send(f"{ctx.author.mention} Devi essere in un canale vocale per mettere la musica"); return
+            else:
+                canale = ctx.author.voice.channel
         voce = get(self.bot.voice_clients, guild=ctx.guild)
 
         if voce and voce.is_connected():
             await voce.move_to(canale)
         else:
             voce = await canale.connect()
+
+        
 
     @commands.command(name="leave")
     async def leave(self,ctx, force=None):
