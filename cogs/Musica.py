a4a62546e48d641778999befa8ed1529c3e21831
Emanuele Borghini <manuborg2002@gmail.com>
2021-06-05 21:24:34 +0200

    rewritten musica

--- cogs/Musica.py
+++ cogs/Musica.py
@@ -1,103 +1,306 @@
-from discord.ext import commands
-from discord.utils import get
-from discord import Member, VoiceChannel, FFmpegPCMAudio
+import itertools
+import asyncio
+from async_timeout import timeout
+from functools import partial
 from youtube_dl import YoutubeDL
+from discord.ext import commands
+from discord import Embed, FFmpegPCMAudio, HTTPException, PCMVolumeTransformer
 
-class Music(commands.Cog):
+ytdlopts = {
+    'format': 'bestaudio/best',
+    'outtmpl': 'downloads/%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'  # ipv6 addresses cause issues sometimes
+}
+
+ffmpegopts = {
+    'before_options': '-nostdin',
+    'options': '-vn'
+}
+
+ytdl = YoutubeDL(ytdlopts)
+
+class YTDLSource(PCMVolumeTransformer):
+
+    def __init__(self, source, *, data, requester):
+        super().__init__(source)
+        self.requester = requester
+
+        self.title = data.get('title')
+        self.web_url = data.get('webpage_url')
+
+        # https://github.com/rg3/youtube-dl/blob/master/README.md
+
+    def __getitem__(self, item: str):
+        return self.__getattribute__(item)
+
+    @classmethod
+    async def create_source(cls, ctx, search: str, *, loop, download=False):
+        loop = loop or asyncio.get_event_loop()
+
+        to_run = partial(ytdl.extract_info, url=search, download=download)
+        data = await loop.run_in_executor(None, to_run)
+
+        if 'entries' in data:
+            data = data['entries'][0]
+
+        await ctx.send(f'```ini\n[ Aggiunto {data["title"]} alla coda ]\n```')
+
+        if download:
+            source = ytdl.prepare_filename(data)
+        else:
+            return {'webpage_url': data['webpage_url'], 'requester': ctx.author, 'title': data['title']}
+
+        return cls(FFmpegPCMAudio(source), data=data, requester=ctx.author)
+
+    @classmethod
+    async def regather_stream(cls, data, *, loop):
+        loop = loop or asyncio.get_event_loop()
+        requester = data['requester']
+
+        to_run = partial(ytdl.extract_info, url=data['webpage_url'], download=False)
+        data = await loop.run_in_executor(None, to_run)
+
+        return cls(FFmpegPCMAudio(data['url']), data=data, requester=requester)
+
+
+class MusicPlayer(commands.Cog):
+    __slots__ = ('bot', '_guild', '_channel', '_cog', 'queue', 'next', 'current', 'np', 'volume')
+
+    def __init__(self, ctx):
+        self.bot = ctx.bot
+        self._guild = ctx.guild
+        self._channel = ctx.channel
+        self._cog = ctx.cog
+
+        self.queue = asyncio.Queue()
+        self.next = asyncio.Event()
+
+        self.np = None  # Now playing message
+        self.volume = .5
+        self.current = None
+
+        ctx.bot.loop.create_task(self.player_loop())
+
+    async def player_loop(self):
+        await self.bot.wait_until_ready()
+
+        while not self.bot.is_closed():
+            self.next.clear()
+
+            try:
+                async with timeout(300):
+                    source = await self.queue.get()
+            except asyncio.TimeoutError:
+                return self.destroy(self._guild)
+
+            if not isinstance(source, YTDLSource):
+                try:
+                    source = await YTDLSource.regather_stream(source, loop=self.bot.loop)
+                except Exception as e:
+                    await self._channel.send(f"C'è stato un errore nella richiesta della canzone.\n"f'```css\n[{e}]\n```')
+                    continue
+
+            source.volume = self.volume
+            self.current = source
+
+            self._guild.voice_client.play(source, after=lambda _: self.bot.loop.call_soon_threadsafe(self.next.set))
+            self.np = await self._channel.send(f'**In riproduzione:** `{source.title}` ' f'[`{source.requester}`]')
+            await self.next.wait()
+
+            source.cleanup()
+            self.current = None
+
+            try:
+                await self.np.delete()
+            except HTTPException:
+                pass
+
+    def destroy(self, guild):
+        return self.bot.loop.create_task(self._cog.cleanup(guild))
+
+
+class Musica(commands.Cog):
+    __slots__ = ('bot', 'players')
+
     def __init__(self, bot):
         self.bot = bot
+        self.players = {}
 
-        self.is_playing = False
-        self.voce = None
+    async def cleanup(self, guild):
+        try:
+            await guild.voice_client.disconnect()
+        except AttributeError:
+            pass
 
-        self.music_queue = []
-        self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
-        self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        try:
+            del self.players[guild.id]
+        except KeyError:
+            pass
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print("Musica caricata!")
+    def get_player(self, ctx):
+        try:
+            player = self.players[ctx.guild.id]
+        except KeyError:
+            player = MusicPlayer(ctx)
+            self.players[ctx.guild.id] = player
 
-    def search_yt(self, item):
-        with YoutubeDL(self.YDL_OPTIONS) as ydl:
-            try: 
-                info = ydl.extract_info("ytsearch:%s" % item, download=False)['entries'][0]
-            except Exception: 
-                return False
+        return player
 
-        return {'source': info['formats'][0]['url'], 'title': info['title']}
+    #@commands.command(name='connect', aliases=['join'])
+    async def connect_(self, ctx):
+        try:
+            channel = ctx.author.voice.channel
+        except AttributeError:
+            await ctx.send('Nessun canale in cui entrare.'); return
 
-    def next(self):
-        if len(self.music_queue) > 0:
-            self.is_playing = True
-            m_url = self.music_queue[0][0]['source']
-            self.music_queue.pop(0)
-            self.voce.play(FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.next())
+        vc = ctx.voice_client
+
+        if vc:
+            if vc.channel.id == channel.id:
+                return
+            try:
+                await vc.move_to(channel)
+            except asyncio.TimeoutError:
+                await ctx.send(f'Spostamento canale: <{channel}> timed out.'); return
         else:
-            self.is_playing = False
+            try:
+                await channel.connect()
+            except asyncio.TimeoutError:
+                await ctx.send(f'Connessione al canale: <{channel}> timed out.'); return
+        embed = Embed(title="Entrato in chiamata")
+        embed.add_field(name="Connesso a:", value=channel, inline=True)
 
-    @commands.command(name="play")
-    async def play(self, ctx, *args):        
-        canale = ctx.author.voice.channel
-        if canale is None:
-            await ctx.send(f"{ctx.author.mention} Devi essere in un canale vocale per mettere la musica"); return
+        #await ctx.send(embed=embed)
 
-        if len(args) == 0:
-            if self.voce is None or not self.voce.is_connected():
-                self.voce = await canale.connect()
-            if self.voce.is_paused():
-                self.voce.resume()                
-        else:
-            query = " ".join(args)
-            canzone = self.search_yt(query)
+    @commands.command(name='play', aliases=['riproduci', 'p'], help='Riproduci una canzone')
+    async def play_(self, ctx, *, search: str):
+        await ctx.trigger_typing()
 
-            await ctx.send("Song added to the queue")
-            self.music_queue.append([canzone, canale])
+        vc = ctx.voice_client
 
-            if self.is_playing == False:
-                if self.voce is None or not self.voce.is_connected():
-                    self.voce = await self.music_queue[0][1].connect()
-                else:
-                    await self.voce.move_to(self.music_queue[0][1])
-                self.next()
+        if not vc:
+            await ctx.invoke(self.connect_)
 
-    @commands.command(name="leave")
-    async def leave(self,ctx, force=None):
-        canale = ctx.author.voice.channel
-        voce = get(self.bot.voice_clients, guild=ctx.guild)
+        player = self.get_player(ctx)
 
-        if force == "f" or canale and canale == voce.channel:
-            self.is_playing = False
-            await voce.disconnect()
-        else:
-            await ctx.send("Devi essere nello stesso canale vocale per disconnettermi")
+        source = await YTDLSource.create_source(ctx, search, loop=self.bot.loop, download=False)
 
-    @commands.command(name="skip", help="Salta la canzone corrente")
-    async def skip(self, ctx):
-        if len(self.music_queue) > 0:
-            if self.voce:
-                self.voce.stop()
-                await self.next()
-        else:
-            await ctx.send("Cosa skippo se non c'è niente nella codaaaa")
+        await player.queue.put(source)
 
-    @commands.command(name="pause", help="Pausa la canzone corrente")
-    async def pause(self, ctx):
-        if self.is_playing:
-            self.voce.pause()
-        else:
-            await ctx.send("Cosa pauso se non c'è niente in riproduzioneeeeee")
+    @commands.command(name='pausa', aliases=['pause'], help="Pausa la canzone in riproduzione")
+    async def pause_(self, ctx):
+        vc = ctx.voice_client
 
-    @commands.command(name="queue", help="Mostra la coda delle canzoni")
-    async def queue(self, ctx):
-        queue = ""
-        for i in range(0, len(self.music_queue)):
-            queue += self.music_queue[i][0]['title'] + "\n"
+        if not vc or not vc.is_playing():
+            return await ctx.send("Non c'è niente in riproduzione!")
+        elif vc.is_paused():
+            await ctx.invoke(self.resume_)
+            return
 
-        if queue != "":
-            await ctx.send(queue)
-        else:
-            await ctx.send("La coda è vuota")
+        vc.pause()
+        await ctx.send(f'**`{ctx.author}`** ha pausato la canzone')
+
+    @commands.command(name='riprendi', aliases=['unpause', 'resume'], help="Riprendi la riproduzione della canzone")
+    async def resume_(self, ctx):
+        vc = ctx.voice_client
+
+        if not vc or not vc.is_connected():
+            return await ctx.send("Non c'è niente in riproduzione!", )
+        elif not vc.is_paused():
+            return
+
+        vc.resume()
+        await ctx.send(f'**`{ctx.author}`** ha ripreso la riproduzione')
+
+    @commands.command(name='skip', aliases=['next'], help="Salta la canzone corrente")
+    async def skip_(self, ctx):
+        vc = ctx.voice_client
+
+        if not vc or not vc.is_connected():
+            return await ctx.send("Non c'è niente in riproduzione!")
+
+        if vc.is_paused():
+            pass
+        elif not vc.is_playing():
+            return
+
+        vc.stop()
+        await ctx.send(f'**`{ctx.author}`**: ha saltato la canzone')
+
+    @commands.command(name='coda', aliases=['q', 'playlist', 'queue'], help="Mostra la coda delle canzoni")
+    async def queue_info(self, ctx):
+        vc = ctx.voice_client
+
+        if not vc or not vc.is_connected():
+            return await ctx.send('Non sono connesso a un canale vocale!')
+
+        player = self.get_player(ctx)
+        if player.queue.empty():
+            return await ctx.send('Non ci sono altre canzoni nella coda.')
+
+        upcoming = list(itertools.islice(player.queue._queue, 0, 5))
+
+        fmt = '\n'.join(f'**`{_["title"]}`**' for _ in upcoming)
+        embed = Embed(title=f'Prossima canzone: {len(upcoming)}', description=fmt)
+
+        await ctx.send(embed=embed)
+
+    @commands.command(name='in_riproduzione', aliases=['np', 'current', 'currentsong', 'playing', 'ir'], help="Mostra la canzone in riproduzione")
+    async def now_playing_(self, ctx):
+        vc = ctx.voice_client
+
+        if not vc or not vc.is_connected():
+            return await ctx.send('Non sono connesso a un canale vocale!', )
+
+        player = self.get_player(ctx)
+        if not player.current:
+            return await ctx.send("Non c'è niente in riproduzione!")
+
+        try:
+            await player.np.delete()
+        except HTTPException:
+            pass
+
+        player.np = await ctx.send(f'**Ora in riproduzione:** `{vc.source.title}` 'f'[`{vc.source.requester}`]') 
+
+    @commands.command(name='volume', aliases=['vol'], help="Cambia il volume della musica")
+    async def change_volume(self, ctx, *, vol: float):
+        vc = ctx.voice_client
+
+        if not vc or not vc.is_connected():
+            return await ctx.send('Non sono connesso a un canale vocale!', )
+
+        if not 0 < vol < 101:
+            return await ctx.send('Inserisci un valora compreso tra 1 e 100')
+
+        player = self.get_player(ctx)
+
+        if vc.source:
+            vc.source.volume = vol / 100
+
+        player.volume = vol / 100
+        embed = Embed(title="Volume",
+        description=f'Volume cambiato da **{ctx.author.name}**')
+        embed.add_field(name="Volume: ", value=vol, inline=True)
+        await ctx.send(embed=embed)
+        # await ctx.send(f'**`{ctx.author}`**: Set the volume to **{vol}%**')
+
+    @commands.command(name='esci', aliases=['stop','leave','fuori'], help="Stoppa la musica (rimuove la coda)")
+    async def stop_(self, ctx):
+        vc = ctx.voice_client
+
+        if not vc or not vc.is_connected():
+            return await ctx.send("Non c'è niente in riproduzione!")
+
+        await self.cleanup(ctx.guild)
 
 def setup(bot):
-    bot.add_cog(Music(bot))
+    bot.add_cog(Musica(bot))
