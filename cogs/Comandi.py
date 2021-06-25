ab50e9f95171b75bd8388285c0343a47c2a904a6
Emanuele Borghini <manuborg2002@gmail.com>
2021-06-26 00:00:22 +0200

    aggiunto comando per misurare la temperatura del raspberry

--- cogs/Comandi.py
+++ cogs/Comandi.py
@@ -8,17 +8,32 @@ class Comandi(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+
     @commands.Cog.listener()
     async def on_ready(self):        
         print("Comandi caricati!")
+
+
+    @commands.command(name="temperatura", aliases=['temp'], help="Mostra la temperatura del raspberry")
+    async def temperatura(self, ctx):
+        try:
+            with open('/sys/class/thermal/thermal_zone0/temp') as file:
+                temperatura = float(file.readline(3))/10
+                emoji = ':fire:' if temperatura > 56 else ':ice:'
+                await ctx.send(f"*Temperatura:* **{temperatura}***°C*  {emoji}")
+        except:
+            await ctx.send(f"C'è stato un errore nel leggere la temperatura, il bot sta eseguendo sul raspberry? :thinking:")
+
 
     @commands.command(name="chisono", aliases=['whoami'], help="Ti ricorda chi sei veramente")
     async def chisono(self, ctx):
         await ctx.send(f"Sei un trimone {ctx.message.author.name}")
 
+
     @commands.command(name="scorreggia", aliases=["scoreggia", "cagati"], help="Comando inutile, provare per credere")
     async def scorreggia(self, ctx):
         await ctx.send(f"Oh no mi sono cagato addosso")
+
 
     @commands.command(name="pinga", aliases=["pinga_utente", "ping"], help="Pinga un utente")
     async def pinga(self, ctx, user : Member, amount=1): 
@@ -27,6 +42,7 @@ class Comandi(commands.Cog):
         else:                
             for x in range(0, amount):
                 await ctx.send(f"{user.mention}")
+
 
     @commands.command(name="raduna", aliases=["radunata", "meeting"], help="Raduna tutti gli utenti in chat vocale nello stesso canale")
     @commands.has_permissions(administrator=True)
@@ -41,33 +57,6 @@ class Comandi(commands.Cog):
             for member in channel.members:
                 await member.move_to(canale)
 
-    @commands.command(name="shakera", aliases=["scuoti"], help="Shakera tutti gli utenti in chat vocale tra alcuni canali")
-    @commands.has_permissions(administrator=True)
-    async def shakera(self, ctx, volte, *canali:VoiceChannel):
-        if len(canali) < 2:
-            return await ctx.send(f"{ctx.author.mention} Devi specificare almeno due canali in cui vuoi shakerare la gente")
-        
-        utenti_chat_vocale = []
-        for channel in ctx.guild.voice_channels:
-            for member in channel.members:
-                utenti_chat_vocale.append(member)
-
-        ultimo_utente = utente_random = None
-        for i in range(0,int(volte)*2*len(utenti_chat_vocale)):
-            while ultimo_utente == utente_random:
-                utente_random = random.choice(utenti_chat_vocale)
-            ultimo_utente = utente_random
-
-            canale_random = random.choice(canali)
-            while canale_random == utente_random.voice:
-                canale_random = random.choice(canali)
-
-            await utente_random.move_to(canale_random)
-            await sleep(0.1)
-
-        for member in utenti_chat_vocale:
-            await member.move_to(canali[0])
-            await sleep(0.1)
 
     @commands.command(name="cancella", aliases=["delete","canc"],help="Cancella # messaggi")
     @commands.has_permissions(manage_messages=True)
@@ -84,6 +73,7 @@ class Comandi(commands.Cog):
                 pass
             if (mode == "giorni" or mode == "d"):
                 pass
+
 
 def setup(bot):
     bot.add_cog(Comandi(bot))
