8df75eba048d08b52163ca944012b4b93f76d28e
Emanuele Borghini <manuborg2002@gmail.com>
2021-10-13 21:03:09 +0200

    added OraEsatta

--- cogs/Loops.py
+++ cogs/Loops.py
@@ -1,6 +1,9 @@
+from os import system
+from discord import File
 from discord.ext import commands, tasks
-from discord import Member, Colour, Permissions
 from discord.utils import get
+from datetime import datetime
+from platform import system
 import random
 
 class Loops(commands.Cog):
@@ -10,6 +13,7 @@ class Loops(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         self.fabio.start()
+        self.oraesatta.start()
         print("Loop caricati!")
 
     #Loop per rinominare Fabio.exe ( id = 192261771380260864 )
@@ -20,6 +24,17 @@ class Loops(commands.Cog):
 
         await fabio.edit(nick=fabio.display_name.split(".")[0]+'.'+random.choice(estensioni))
 
+    @tasks.loop(seconds=59)
+    async def oraesatta(self):
+        orario = datetime.now().strftime("%H:%M")
+        if (orario == "07:15"):
+            file_path = "home/pi/ora_esatta.mp4" if system() == "Linux" else "C:/Users/Borgo/Desktop/ora_esatta.mp4"
+            for id in [805397632494338091]:
+                channel = self.bot.get_channel(id)
+                try:
+                    await channel.send(f"L'ora esatta è offerta da: ***Culo*** :peach: \nSono le **{orario}**", file=File(file_path))
+                except:
+                    await channel.send(f"Non sono riuscito a mandare l'ora esatta :/")
 
 def setup(bot):
     bot.add_cog(Loops(bot))
