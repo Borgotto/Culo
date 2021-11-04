4c6ec1182fc3e94f6eb755874715183ea41b594c
Emanuele Borghini <manuborg2002@gmail.com>
2021-11-04 01:46:27 +0100

    added one more message to respond to

--- cogs/Eventi.py
+++ cogs/Eventi.py
@@ -13,7 +13,7 @@ class Eventi(commands.Cog):
         if message.author == self.bot.user: return
 
         messaggio = message.content.lower()
-        risposta = {'bravo bot':'UwU grazie','luca gay':'si, luca è gay','borgo gay':'no tu sei gay','punta il ferro':'https://imgur.com/BZDUDxp','pablo comunista':'https://imgur.com/mD77kay','pspspsps':'https://imgur.com/TXW54kd','ps ps ps ps':'https://imgur.com/TXW54kd','pspsps':'https://imgur.com/TXW54kd'}
+        risposta = {'bravo bot':'UwU grazie','luca gay':'si, luca è gay','borgo gay':'no tu sei gay','punta il ferro':'https://imgur.com/BZDUDxp','pablo comunista':'https://imgur.com/mD77kay','pspspsps':'https://imgur.com/TXW54kd','ps ps ps ps':'https://imgur.com/TXW54kd','pspsps':'https://imgur.com/TXW54kd','lavatrice':'https://imgur.com/a/NdQfex2'}
         
         if messaggio in risposta:
             await message.channel.send(risposta[messaggio])
