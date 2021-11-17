77af0e60551c4ca708be37298fe344223e4bc1d6
Emanuele Borghini <manuborg2002@gmail.com>
2021-11-17 14:11:59 +0100

    bot now replies to event messages

--- cogs/Eventi.py
+++ cogs/Eventi.py
@@ -14,12 +14,12 @@ class Eventi(commands.Cog):
 
         messaggio = message.content.lower()
         risposta = {'bravo bot':'UwU grazie','luca gay':'si, luca è gay','borgo gay':'no tu sei gay','punta il ferro':'https://imgur.com/BZDUDxp','pablo comunista':'https://imgur.com/mD77kay','pspspsps':'https://imgur.com/TXW54kd','ps ps ps ps':'https://imgur.com/TXW54kd','pspsps':'https://imgur.com/TXW54kd','lavatrice':'https://imgur.com/a/NdQfex2'}
-        
+
         if messaggio in risposta:
-            await message.channel.send(risposta[messaggio])
+            await message.channel.send(risposta[messaggio], reference=message, mention_author=False)
 
         if messaggio == 'prefisso?':
-            await message.channel.send(f"Il prefisso per i comandi è: `{self.bot.command_prefix(self, message)}`")
+            await message.channel.send(f"Il prefisso per i comandi è: `{self.bot.command_prefix(self, message)}`", reference=message, mention_author=False)
 
 def setup(bot):
     bot.add_cog(Eventi(bot))
