9f90a4be1c22b794e675693217c369b8d5508627
Emanuele Borghini <manuborg2002@gmail.com>
2021-11-17 16:46:36 +0100

    better error handling

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import json
 
 ###################
 #    bot setup    #
-################### 
+###################
 def get_token():
     if os.getenv('BOT_TOKEN') is not None:
         return os.environ.get("BOT_TOKEN")
@@ -24,13 +24,13 @@ def get_prefix(client, message):
     if message.guild is None: return '🍑'
     with open('config/prefixes.json', 'r') as file:
         prefixes = json.load(file)
-    return prefixes[str(message.guild.id)]  
+    return prefixes[str(message.guild.id)]
 
 #updates prefixes.json with the right prefixes, deleting unnecessary ones and adding missing ones
 def update_prefixes():
-    with open('config/prefixes.json', 'r') as file: 
-        prefixes = json.load(file)     
-    
+    with open('config/prefixes.json', 'r') as file:
+        prefixes = json.load(file)
+
         updated_prefixes = { }
 
         for guild in bot.guilds:
@@ -39,7 +39,7 @@ def update_prefixes():
             except KeyError:
                 updated_prefixes[str(guild.id)] = '🍑'
 
-    with open('config/prefixes.json', 'w') as file: 
+    with open('config/prefixes.json', 'w') as file:
         json.dump(updated_prefixes, file, indent=4)
 
 #create config folder if it doesn't exist
@@ -64,7 +64,7 @@ bot = commands.Bot(command_prefix = (get_prefix), owner_id=289887222310764545, i
 async def on_ready():
     #updates to bot presence
     await bot.change_presence(status = discord.Status.online, activity = (discord.Activity(name= f"🍑 👀", type=discord.ActivityType.watching)))
-    
+
     #makes sure the different prefixes are up to date with the bot's actual servers
     update_prefixes()
 
@@ -74,9 +74,9 @@ async def on_ready():
     print ("Bot ID:", bot.user.id)
     print ("Discord Version:", discord.__version__)
     print ("\n--------------------------------\n")
-    #print servers info    
-    with open('config/prefixes.json', 'r') as file: 
-        prefixes = json.load(file) 
+    #print servers info
+    with open('config/prefixes.json', 'r') as file:
+        prefixes = json.load(file)
         print(f'The bot is in {len(bot.guilds)} servers!')
         print("List of all servers the bot is in:", end="\n\n")
         for guild in bot.guilds:
@@ -90,24 +90,24 @@ async def on_ready():
 #    prefix handling    #
 #########################
 @bot.event
-async def on_guild_join(guild): 
-    with open('config/prefixes.json', 'r') as file: 
-        prefixes = json.load(file) 
+async def on_guild_join(guild):
+    with open('config/prefixes.json', 'r') as file:
+        prefixes = json.load(file)
 
     prefixes[str(guild.id)] = '🍑' #default prefix
 
-    with open('config/prefixes.json', 'w') as file: 
-        json.dump(prefixes, file, indent=4) 
-    
+    with open('config/prefixes.json', 'w') as file:
+        json.dump(prefixes, file, indent=4)
+
     #updates to bot presence
     #await bot.change_presence(status = discord.Status.online, activity = (discord.Activity(name= f"alle bestemmie di {len(bot.guilds)} server...", type=discord.ActivityType.listening)))
 
 @bot.event
-async def on_guild_remove(guild): 
-    with open('config/prefixes.json', 'r') as file: 
+async def on_guild_remove(guild):
+    with open('config/prefixes.json', 'r') as file:
         prefixes = json.load(file)
 
-    prefixes.pop(str(guild.id)) 
+    prefixes.pop(str(guild.id))
 
     with open('config/prefixes.json', 'w') as file:
         json.dump(prefixes, file, indent=4)
@@ -116,17 +116,17 @@ async def on_guild_remove(guild):
     #await bot.change_presence(status = discord.Status.online, activity = (discord.Activity(name= f"alle bestemmie di {len(bot.guilds)} server...", type=discord.ActivityType.listening)))
 
 @bot.command(name="cambia_prefisso", aliases=["prefisso"], help="Cambia il prefisso per i comandi del bot")
-@commands.has_permissions(administrator=True) 
-async def cambia_prefisso(ctx, prefisso : str): 
+@commands.has_permissions(administrator=True)
+async def cambia_prefisso(ctx, prefisso : str):
     with open('config/prefixes.json', 'r') as file:
         prefixes = json.load(file)
 
     prefixes[str(ctx.guild.id)] = prefisso
 
-    with open('config/prefixes.json', 'w') as file: 
+    with open('config/prefixes.json', 'w') as file:
         json.dump(prefixes, file, indent=4)
 
-    await ctx.send(f'Prefisso cambiato a: {prefisso}') 
+    await ctx.send(f'Prefisso cambiato a: {prefisso}')
 
 
 
@@ -150,12 +150,12 @@ async def disattiva(ctx, cog : str):
 @commands.is_owner()
 async def ricarica(ctx):
     for filename in os.listdir('./cogs'):
-        if (filename.endswith('.py')):            
+        if (filename.endswith('.py')):
             bot.unload_extension(f'cogs.{filename[:-3]}')
             bot.load_extension(f'cogs.{filename[:-3]}')
     await ctx.send(f'Cog ricaricati!')
 
-#carica tutti i cog all'avvio       
+#carica tutti i cog all'avvio
 for filename in os.listdir('./cogs'):
     if (filename.endswith('.py')):
         bot.load_extension(f'cogs.{filename[:-3]}')
@@ -167,25 +167,27 @@ for filename in os.listdir('./cogs'):
 ################################
 @bot.event
 async def on_command_error(ctx, error):
+    #generic error message, overwritten if a more specific one is found
+    error_message = f"C'è stato qualche errore con il comando :peach: :poop:"
+
     if isinstance(error, commands.CommandNotFound):
-        return await ctx.send(f'Comando sconosciuto :peach: :poop:')
-
+        error_message = f"Comando sconosciuto :peach: :poop:"
     if isinstance(error, commands.MissingPermissions):
-        return await ctx.send(f'Non hai i permessi per usare questo comando...\nCosa pensavi di fare eh? :peach: :poop:')
-
+        error_message = f"Non hai i permessi per usare questo comando...\nCosa pensavi di fare eh? :peach: :poop:"
     if isinstance(error, commands.MissingRequiredArgument):
-        return await ctx.send(f'Manca qualche argomento per usare questo comando... :peach: :thinking:')
+        error_message = f"Manca qualche argomento per usare questo comando... :peach: :thinking:"
 
-    await ctx.send(f"C'è stato qualche errore con il comando :peach: :poop:")
+    await ctx.send(error_message, reference=ctx.message, mention_author=False)
 
+    # a more detailed error is sent to the bot owner
     await bot.get_user(bot.owner_id).send(f"**There was an error with the bot!**\n`{error.original}`\n\n\
     **Server:**  `{ctx.guild.name} [{ctx.guild.id}]`\n\
     **Channel:**  `{ctx.channel.name} [{ctx.channel.id}]`\n\
     **Command:**  `{ctx.command.name}`\n\
-    **Arguments:**  `{ctx.message.content.split(ctx.command.name)[1][1:]}`\n\
+    **Arguments:**  `{ctx.message.content.split(ctx.command.name)}`\n\
     **Command author:**  {ctx.message.author.mention} `[{ctx.message.author.id}]`\n\
     **Date:**  `{ctx.message.created_at.strftime('%d/%m/%Y - %H:%M:%S')}`\n\
-")  
+", allowed_mentions=False)
 
 
 ############################
