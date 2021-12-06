db215c0c86573194a2886c15b3b391417025c9fb
Emanuele Borghini <manuborg2002@gmail.com>
2021-12-06 17:48:53 +0100

    added message to respond to

--- cogs/Eventi.py
+++ cogs/Eventi.py
@@ -13,7 +13,7 @@ class Eventi(commands.Cog):
         if message.author == self.bot.user: return
 
         messaggio = message.content.lower()
-        risposta = {'bravo bot':'UwU grazie','luca gay':'si, luca è gay','borgo gay':'no tu sei gay','stefano sei un ciccione':'sta zitto Nicò','punta il ferro':'https://imgur.com/BZDUDxp','pablo comunista':'https://imgur.com/mD77kay','pspspsps':'https://imgur.com/TXW54kd','ps ps ps ps':'https://imgur.com/TXW54kd','pspsps':'https://imgur.com/TXW54kd','lavatrice':'https://imgur.com/a/NdQfex2'}
+        risposta = {'bravo bot':'UwU grazie','luca gay':'si, luca è gay','borgo gay':'no tu sei gay','stefano sei un ciccione':'sta zitto Nicò','ciao ste':'ciao ste','punta il ferro':'https://imgur.com/BZDUDxp','pablo comunista':'https://imgur.com/mD77kay','pspspsps':'https://imgur.com/TXW54kd','ps ps ps ps':'https://imgur.com/TXW54kd','pspsps':'https://imgur.com/TXW54kd','lavatrice':'https://imgur.com/a/NdQfex2'}
 
         if messaggio in risposta:
             await message.channel.send(risposta[messaggio], reference=message, mention_author=False)
